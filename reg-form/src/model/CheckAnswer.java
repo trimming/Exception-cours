abcef08465a8cc23f14aa131a8e1015bd7b980ba
Alexey Golikov <lexgolikoff@gmail.com>
2023-11-02 19:48:36 +0300

    added some comments

--- reg-form/src/model/CheckAnswer.java
+++ reg-form/src/model/CheckAnswer.java
@@ -14,6 +14,12 @@ public class CheckAnswer {
         this.answerUserArray = getAnswerUserArray();
     }
 
+    /**
+     * Преобразует строку с ответом пользователя в массив, проверяет количество данных
+     * @return массив строк данных
+     * @throws InsufficientDataException Недостаточно данных!
+     * @throws ExtraInfoException Лишняя информация!
+     */
     public String[] getAnswerUserArray() {
         try {
             answerUserArray = answer.getAnswer().split(" ");
@@ -30,6 +36,11 @@ public class CheckAnswer {
         return answerUserArray;
     }
 
+    /**
+     * Разбирает элементы массива с данными от пользователя на соответствие информации о поле
+     * @return возвращает символ соответствующий полу или null, если нет соответствия
+     * @throws ParseGenderException Отсутствует значение пола!
+     */
     public Character parseUserAnswerToGender(){
         Character gender = null;
         try {
@@ -49,6 +60,11 @@ public class CheckAnswer {
         }
         return null;
     }
+    /**
+     * Разбирает элементы массива с данными от пользователя на соответствие информации о номере телефона
+     * @return возвращает число соответствующее номеру телефона или null, если нет соответствия
+     * @throws ParseTelNumberException некорректный номер телефона
+     */
     public Long parseUserAnswerToTelNumber() throws ParseTelNumberException{
         Long telNumber = null;
         try{
@@ -63,13 +79,18 @@ public class CheckAnswer {
             if(!res){
                 throw new ParseTelNumberException("Некорректный номер телефона!");
             }
-
             }
         catch (ParseTelNumberException e){
             System.out.println(e.getMessage());
         }
         return null;
     }
+
+    /**
+     * Разбирает элементы массива с данными от пользователя на соответствие информации о имени
+     * @return возвращает строку соответствующее имени или null, если нет соответствия
+     * @throws ParseNameException Некорректное значение имени!
+     */
     public String parseUserAnswerToName(){
         StringBuilder name = new StringBuilder();
         try{
@@ -91,6 +112,11 @@ public class CheckAnswer {
         }
         return null;
     }
+    /**
+     * Разбирает элементы массива с данными от пользователя на соответствие информации о дате рождения
+     * @return возвращает дату или null, если нет соответствия
+     * @throws ParseBirthDateException Некорректная дата дня рождения!
+     */
     public Date parseUserAnswerToBirthDate() {
         Date birthDate = null;
         DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
@@ -111,6 +137,11 @@ public class CheckAnswer {
         }
         return null;
     }
+
+    /**
+     * Разбирает элементы массива с данными от пользователя на соответствие информации о готовности выйти из приложения
+     * @return true или false
+     */
     public boolean parseUserAnswerToQuit(){
         for (String answer : this.answerUserArray) {
             if (answer.equals("q")) {
--- reg-form/src/service/DataService.java
+++ reg-form/src/service/DataService.java
@@ -15,20 +15,23 @@ public class DataService {
 
     public DataService() {
         this.request = new MakeRequestToUser(scanner);
-//        this.checkAnswer = new CheckAnswer(request.getMakeUserAnswer());
         this.stage = true;
     }
 
+    /**
+     * Проверяет данные пользователя по всем критериям соответствия, записывает данные в файл
+     * @throws AssertionError Данные не могут быть записаны, т.к. некорректное значение имени!
+     */
     public void createUser(){
         while(stage){
             try {
-
                 checkAnswer = new CheckAnswer(request.getMakeUserAnswer());
+                stage = checkAnswer.parseUserAnswerToQuit();
                 String name = checkAnswer.parseUserAnswerToName();
                 Date birthDate = checkAnswer.parseUserAnswerToBirthDate();
                 Long telNumber = checkAnswer.parseUserAnswerToTelNumber();
                 Character gender = checkAnswer.parseUserAnswerToGender();
-                stage = checkAnswer.parseUserAnswerToQuit();
+
                 if (name == null) throw new AssertionError("Данные не могут быть записаны, т.к. некорректное значение имени!");
 
                 WriteUserData writeUserData = new WriteUserData(new User(name, birthDate, telNumber, gender));
